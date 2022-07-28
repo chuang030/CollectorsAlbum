8d2c1da47dcf79fcf02a668bcd033f4d64c2ef92
Toma <novotny.tom96@gmail.com>
2022-07-29 00:06:31 +0200

    Added class for item code generation

---
+++ src/main/java/team/tnt/collectoralbum/util/datagen/CardItemCodeGen.java
@@ -0,0 +1,16 @@
+package team.tnt.collectoralbum.util.datagen;
+
+public class CardItemCodeGen {
+
+    public static void main(String[] args) {
+        try {
+            new CardItemCodeGen().run();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void run() {
+        // TODO
+    }
+}
