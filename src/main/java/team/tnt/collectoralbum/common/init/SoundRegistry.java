6adf2a1d95655b40d808b4cecd6392aa80a49338
Tomano <vetom@centrum.cz>
2022-08-03 22:55:02 +0200

    sound registry

--- src/main/java/team/tnt/collectoralbum/common/init/SoundRegistry.java
+++ src/main/java/team/tnt/collectoralbum/common/init/SoundRegistry.java
@@ -7,10 +7,24 @@ import team.tnt.collectoralbum.CollectorsAlbum;
 
 public final class SoundRegistry {
 
-    public static void registerSounds() {}
+    public static final SoundEvent FLIP_COMMON = new SoundEvent(new ResourceLocation(CollectorsAlbum.MODID, "flip_common"));
+    public static final SoundEvent FLIP_UNCOMMON = new SoundEvent(new ResourceLocation(CollectorsAlbum.MODID, "flip_uncommon"));
+    public static final SoundEvent FLIP_RARE = new SoundEvent(new ResourceLocation(CollectorsAlbum.MODID, "flip_rare"));
+    public static final SoundEvent FLIP_EPIC = new SoundEvent(new ResourceLocation(CollectorsAlbum.MODID, "flip_epic"));
+    public static final SoundEvent FLIP_LEGENDARY = new SoundEvent(new ResourceLocation(CollectorsAlbum.MODID, "flip_legendary"));
+    public static final SoundEvent FLIP_MYTHICAL = new SoundEvent(new ResourceLocation(CollectorsAlbum.MODID, "flip_mythical"));
 
-    private static void registerSound(String id, SoundEvent event) {
-        registerSound(new ResourceLocation(CollectorsAlbum.MODID, id), event);
+    public static void registerSounds() {
+        registerSound(FLIP_COMMON);
+        registerSound(FLIP_UNCOMMON);
+        registerSound(FLIP_RARE);
+        registerSound(FLIP_EPIC);
+        registerSound(FLIP_LEGENDARY);
+        registerSound(FLIP_MYTHICAL);
+    }
+
+    private static void registerSound(SoundEvent event) {
+        registerSound(event.getLocation(), event);
     }
 
     private static void registerSound(ResourceLocation id, SoundEvent event) {
