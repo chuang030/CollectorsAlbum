55c9055d3fb0a0c8096cee6146f9605dee5c73ab
Toma <novotny.tom96@gmail.com>
2022-08-09 23:09:58 +0200

    Fixed album

--- src/main/java/team/tnt/collectoralbum/common/AlbumBoostHandler.java
+++ src/main/java/team/tnt/collectoralbum/common/AlbumBoostHandler.java
@@ -23,18 +23,24 @@ public class AlbumBoostHandler {
     }
 
     private void tickPlayer(ServerPlayer player) {
-        if (player.level.getGameTime() % 25L != 0L) return;
+        if (player.level.getGameTime() % 50L != 0L) return;
         AbstractContainerMenu inventory = player.containerMenu;
+        boolean hasAlbum = false;
         for (ItemStack stack : inventory.getItems()) {
             if (stack.getItem() == ItemRegistry.ALBUM) {
                 applyBoosts(stack, player);
+                hasAlbum = true;
                 break;
             }
+        }
+        if (!hasAlbum) {
+            player.removeEffect(MobEffects.HEALTH_BOOST);
         }
     }
 
     private void applyBoosts(ItemStack album, ServerPlayer player) {
         AlbumStats stats = AlbumStats.createSimplifiedWithoutContainer(album);
+        int effectTime = 60;
         int points = stats.getPoints();
         int healthBoostModifier = -1;
         if (points >= 900) {
@@ -50,16 +56,20 @@ public class AlbumBoostHandler {
         } else if (points >= 300) {
             healthBoostModifier = 0;
         }
-        if (healthBoostModifier >= 0) {
-            player.addEffect(new MobEffectInstance(MobEffects.HEALTH_BOOST, 30, healthBoostModifier, false, false));
+        MobEffectInstance instance = player.getEffect(MobEffects.HEALTH_BOOST);
+        if (instance != null && healthBoostModifier < 0) {
+            player.removeEffect(MobEffects.HEALTH_BOOST);
+        } else if (healthBoostModifier >= 0 && (instance != null && instance.getAmplifier() != healthBoostModifier)) {
+            player.removeEffect(MobEffects.HEALTH_BOOST);
+            player.addEffect(new MobEffectInstance(MobEffects.HEALTH_BOOST, Integer.MAX_VALUE, healthBoostModifier, false, false));
         }
         int potionModifier = points >= 900 ? 1 : 0;
         Map<CardCategory, Integer> byCategories = stats.getCardsByCategory();
-        applyBoosts(byCategories, CardCategory.TOOLS, player, serverPlayer -> serverPlayer.addEffect(new MobEffectInstance(MobEffects.DIG_SPEED, 30, potionModifier, false, false)));
-        applyBoosts(byCategories, CardCategory.ARMOR, player, serverPlayer -> serverPlayer.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 30, potionModifier, false, false)));
-        applyBoosts(byCategories, CardCategory.MOBS, player, serverPlayer -> serverPlayer.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 30, potionModifier, false, false)));
-        applyBoosts(byCategories, CardCategory.NATURE, player, serverPlayer -> serverPlayer.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 30, potionModifier, false, false)));
-        applyBoosts(byCategories, CardCategory.ITEMS, player, serverPlayer -> serverPlayer.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 30, potionModifier, false, false)));
+        applyBoosts(byCategories, CardCategory.TOOLS, player, serverPlayer -> serverPlayer.addEffect(new MobEffectInstance(MobEffects.DIG_SPEED, effectTime, potionModifier, false, false)));
+        applyBoosts(byCategories, CardCategory.ARMOR, player, serverPlayer -> serverPlayer.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, effectTime, potionModifier, false, false)));
+        applyBoosts(byCategories, CardCategory.MOBS, player, serverPlayer -> serverPlayer.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, effectTime, potionModifier, false, false)));
+        applyBoosts(byCategories, CardCategory.NATURE, player, serverPlayer -> serverPlayer.addEffect(new MobEffectInstance(MobEffects.REGENERATION, effectTime, potionModifier, false, false)));
+        applyBoosts(byCategories, CardCategory.ITEMS, player, serverPlayer -> serverPlayer.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, effectTime, potionModifier, false, false)));
     }
 
     private void applyBoosts(Map<CardCategory, Integer> data, CardCategory category, ServerPlayer player, Consumer<ServerPlayer> event) {
