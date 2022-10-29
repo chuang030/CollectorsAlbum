e1a9ca3e2ea8a24707caf09a12cbc1e3a46b8efd
Toma <novotny.tom96@gmail.com>
2022-10-29 17:56:45 +0200

    Fixed issue with missing boosts

--- src/main/java/team/tnt/collectoralbum/common/AlbumBoostHandler.java
+++ src/main/java/team/tnt/collectoralbum/common/AlbumBoostHandler.java
@@ -1,26 +1,31 @@
 package team.tnt.collectoralbum.common;
 
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.players.PlayerList;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import team.tnt.collectoralbum.CollectorsAlbum;
 import team.tnt.collectoralbum.common.init.ItemRegistry;
 import team.tnt.collectoralbum.data.boosts.*;
 
 import java.util.Optional;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = CollectorsAlbum.MODID)
 public class AlbumBoostHandler {
 
-    public void onServerTick(MinecraftServer server) {
-        PlayerList playerList = server.getPlayerList();
-        for (ServerPlayer player : playerList.getPlayers()) {
-            tickPlayer(player);
-        }
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        Player entity = event.player;
+        if (event.phase == TickEvent.Phase.START || entity.level.isClientSide)
+            return;
+        ServerPlayer serverPlayer = (ServerPlayer) entity;
+        tickPlayer(serverPlayer);
     }
 
-    private void tickPlayer(ServerPlayer player) {
+    private static void tickPlayer(ServerPlayer player) {
         if (player.level.getGameTime() % 50L != 0L) return;
         AbstractContainerMenu inventory = player.containerMenu;
         Optional<AlbumCardBoostCollection> data = CollectorsAlbum.ALBUM_CARD_BOOST_MANAGER.getBoosts();
@@ -40,7 +45,7 @@ public class AlbumBoostHandler {
         });
     }
 
-    private void applyBoosts(ItemStack album, ServerPlayer player, AlbumCardBoostCollection ops) {
+    private static void applyBoosts(ItemStack album, ServerPlayer player, AlbumCardBoostCollection ops) {
         AlbumStats stats = AlbumStats.createSimplifiedWithoutContainer(album);
         IBoostContext ctx = new ActiveBoostContext(player, stats);
         ops.processOp(OpType.ACTIVE, ctx);
